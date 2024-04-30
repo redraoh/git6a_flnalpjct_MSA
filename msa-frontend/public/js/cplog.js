4de0eae16eee420b3b79dfc7a0e0fec2b3cde2a9
YeEul, Kwon <gruuriii@gmail.com>
2024-04-30 19:36:29 +0900

    nav 출력만

--- msa-frontend/public/js/cplog.js
+++ msa-frontend/public/js/cplog.js
@@ -29,19 +29,16 @@ dtrdo.addEventListener('change', () => {
 // 할인권 전체 정보 조회
 const getCouponInfo = async () => {
     const alres = await fetch(`http://127.0.0.1:8040/coupons`)
-    const res = await fetch(`http://127.0.0.1:8040/coupons/1`)
     if (alres.ok) {
         const aldata = await alres.json()
-        const data = await res.json()
-        return aldata, data;
+        return aldata;
     } else {
         throw new Error('전체 할인권 정보 조회 실패!');
     }
 };
 
-
 // 할인권 첫페이지 정보 조회
-const getCouponInfoFirstPage = async () => {
+const getCouponInfoPage = async () => {
     const res = await fetch(`http://127.0.0.1:8040/coupons/1`)
     if (res.ok) {
         const data = await res.json()
@@ -87,13 +84,90 @@ const displayCouponInfo = (coupons) => {
     couponlist.innerHTML = html;
 };
 
+// nav test
+const displayNavigation = (alcps) => {
+    const navuilist = document.querySelector('#navui');
+
+    let cpg = 1;
+    let stpg = Math.floor((cpg - 1) / 10) * 10 + 1;
+    let cnt = alcps.length;
+    let allpage = Math.ceil(cnt / 10);
+
+    console.log(allpage);
+
+    let html = `
+        <li class="disabled">
+            <a
+                class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 gap-1 pl-2.5"
+                aria-label="Go to previous page"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="h-4 w-4"
+                >
+                    <path d="m15 18-6-6 6-6"></path>
+                </svg>
+                <span>Previous</span>
+            </a>
+        </li>
+    `;
+    if (allpage > 1) {
+        for (let i = 0; i < allpage; i++) {
+            html += `
+                <li class="page-item active">
+                    <a class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10"
+                       href="">${i + 1}</a>
+                </li>
+            `;
+        }
+    } else {
+        html += ``;
+    }
+    html += `
+        <li class="">
+            <a
+                class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 gap-1 pr-2.5"
+                aria-label="Go to next page"
+            >
+                <span>Next</span>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="h-4 w-4"
+                >
+                    <path d="m9 18 6-6-6-6"></path>
+                </svg>
+            </a>
+        </li>
+    `;
+    navuilist.innerHTML = html;
+};
+
 
 // 페이지 로드시 실행
 window.addEventListener('load', async () => {
     try {
-        const coupons = await getCouponInfo();
+        const alcps = await getCouponInfo();
+        const coupons = await getCouponInfoPage();
         displayCouponInfo(coupons);
+        displayNavigation(alcps);
         console.log(coupons);
+        console.log(alcps);
     } catch (e) {
         console.error(e);
         alert('할인권 목록 조회 실패!!');
