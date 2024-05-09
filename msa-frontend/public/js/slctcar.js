3718928c91f43f1c83eda68f92c69e3c83461247
Hoarder <kinskicom@gmail.com>
2024-05-09 15:22:38 +0900

    Update slctcar.js

--- msa-frontend/public/js/slctcar.js
+++ msa-frontend/public/js/slctcar.js
@@ -8,7 +8,7 @@ let schbtn = document.querySelector("#schbtn");
 
 // 입차 내역 검색 조회
 const findCarInfo = async (skey) => {
-    const res = await fetch(`http://54.180.127.84:8000/discount/${skey}`)
+    const res = await fetch(`http://43.203.182.213:8000/discount/${skey}`)
     if (res.ok) {
         const data = await res.json()
         return data;
